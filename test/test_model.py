0d2f24a3e0deb719a905bc9dde5c62d8216f0c65
Fetasalyt <121634381+Fetasalyt@users.noreply.github.com>
2025-03-31 14:41:06 +0200

    Update test_model.py

--- test/test_model.py
+++ test/test_model.py
@@ -5,4 +5,4 @@ M = tcam.TModel(50, 500, 10, 1, 24, 1/24, 15, 4)
 M.run_model(animated = False, stats = True)
    
 # Create a Graphical Interface
-# M.tcam.create_dashboard()
+# M.create_dashboard()
