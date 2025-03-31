f537fa82de799f16dab07255b2cece950d24c6c3
Fetasalyt <121634381+Fetasalyt@users.noreply.github.com>
2025-03-31 14:40:35 +0200

    Update test_model.py
    TCAMpy is no longer in a folder, it is a lone module. Import it like that!

--- test/test_model.py
+++ test/test_model.py
@@ -1,10 +1,8 @@
-import Model.TCAMpy2D as tcam
+import TCAMpy as tcam
 
-# Create a Model
+# Create and Run Model
 M = tcam.TModel(50, 500, 10, 1, 24, 1/24, 15, 4)
-
-# Run Model
 M.run_model(animated = False, stats = True)
-    
+   
 # Create a Graphical Interface
 # M.tcam.create_dashboard()
