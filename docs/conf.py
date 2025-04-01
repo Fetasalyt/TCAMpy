6a47b10307c07b09fb0ee33a457e3f24cdeb9241
Fetasalyt <121634381+Fetasalyt@users.noreply.github.com>
2025-04-01 15:48:47 +0200

    Update conf.py
    Add sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -30,10 +30,10 @@ author = 'Rózsa Richárd Bence'
 extensions = [
   'sphinx.ext.autodoc',
   'sphinx.ext.napoleon',
+  'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -46,7 +46,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
