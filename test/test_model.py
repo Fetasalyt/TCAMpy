23b2a15caed6c643c1fedc56d72c2b64c7aa099e
Fetasalyt <121634381+Fetasalyt@users.noreply.github.com>
2025-04-28 23:22:37 +0200

    Update test_model.py

--- test/test_model.py
+++ test/test_model.py
@@ -2,7 +2,10 @@ import TCAMpy as tcam
 
 # Create and Run Model
 M = tcam.TModel(50, 500, 10, 1, 24, 1/24, 15, 4)
-M.run_model(animated = False, stats = True)
-   
+
+M.run_model(plot = True, animate = True, stats = True)
+
+# ---------- OR ----------
+
 # Create a Graphical Interface
 # M.create_dashboard()
