9aa3b18eb73ebc3761611e4ec25b6f10021ab56a
Fetasalyt <121634381+Fetasalyt@users.noreply.github.com>
2025-05-11 19:16:22 +0200

    Create example_model.py

---
+++ examples/example_model.py
@@ -0,0 +1,13 @@
+import TCAMpy as tcam
+
+# -- Create and Run Model --
+
+M = tcam.TModel(50, 500, 10, 1, 24, 1/24, 15, 4)
+M.run_model(plot = True, animate = True, stats = True)
+
+# -- Run multiple models --
+
+# stats = M.run_multimodel(3, M.field)
+
+# print(stats)
+# M.plot_averages(stats)
