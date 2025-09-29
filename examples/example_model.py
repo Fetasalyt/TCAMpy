406d6c496d2c4b86b5020372f7a5f093d5a79b5f
Fetasalyt <121634381+Fetasalyt@users.noreply.github.com>
2025-09-29 13:50:08 +0200

    Include immune response

--- examples/example_model.py
+++ examples/example_model.py
@@ -2,7 +2,7 @@ import TCAMpy as tcam
 
 # -- Create and Run Model --
 
-M = tcam.TModel(50, 500, 10, 1, 24, 1/24, 15, 4)
+M = tcam.TModel(50, 500, 10, 1, 24, 1/24, 15, 4, 5)
 M.run_model(plot = True, animate = True, stats = True)
 
 # -- Run multiple models --
